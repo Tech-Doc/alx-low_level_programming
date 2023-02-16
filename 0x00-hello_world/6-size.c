f95e403ba3a46ae8919d0428605e0c554facd3a7
Tech-Doc <dr.stanleyperpetual@gmail.com>
2023-02-16 09:18:15 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) 
+	/* main - a program to describe main in c programming */
+{int a; long int b; long long int c; char d; float f;
+
+printf("Size of a char: %1u byte(s)\n", (unsigned long)size of(d));
+printf("Size of an int: %1u byte(s)\n", (unsigned long)size of(a));
+printf("Size of a long int: %1u byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long long int: %1u byte(s)\n", (unsigned long)size of(c));
+printf("Size of a float:%1u byte(s)\n", (unsigned long)size of(f));
+return (0)
+
+	}}
