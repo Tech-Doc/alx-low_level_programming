feaf4d0366b77d2ebd77e4b73797a2e6e1ba43cc
Tech-Doc <dr.stanleyperpetual@gmail.com>
2023-02-16 11:31:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#include<stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return(0); }
+#include <stdio.h>
+
+/**
+ * main - a program that prints using put
+ *
+ * Return: 0 means successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
