d2f9d41b61b43c69cf95d541527ae41f9b70207d
Tech-Doc <dr.stanleyperpetual@gmail.com>
2023-02-21 05:57:33 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+#endif
+}
