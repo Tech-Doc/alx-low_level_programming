84733397d6e640c2f9352eb3ed30024b645b640d
Tech-Doc <dr.stanleyperpetual@gmail.com>
2023-02-21 20:33:24 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,25 +5,25 @@
  */
 
 int main(void)
-{
-	int i = 0;
-        long j = 1, k = 2;
-	
-	while (i < 50)
-        {
-		if (i == 0)
-			printf("%ld", j);
-                else if (i == 1)
-                        printf(", %ld", k);
-                else
-                {
-                        k += j;
-                        j = k - j;
-                        printf(", %ld", k);
-                }
-                ++i;
-        }
-        printf("\n");
-        return (0);
-}
+	{
+	int counter = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
 
+	printf("%ld, %ld, ", a, b);
+	while (counter < 50)
+	{
+		printf("%ld", c);
+		counter++;
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
